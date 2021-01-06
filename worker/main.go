33be7d98abb0707ab150fc673b7ff488b3c0cf00
Jarek Lupinski <jarek@lupin.ski>
2021-01-05 21:49:29 -0500

    enable multiple workers

--- worker/main.go
+++ worker/main.go
@@ -2,31 +2,46 @@ package main
 
 import (
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 	_ "github.com/heroku/x/hmetrics/onload"
+
 	"watchdog-email/util"
 )
 
-func checkWatchdog(r *util.RedisController) {
+func checkWatchdog(r *util.RedisController) bool {
 	rds := r.GetRedisConnection()
 	now := time.Now().Unix()
 
-	emailAddresses, err := redis.Strings(rds.Do("ZRANGEBYSCORE", "email", 0, now))
+	redisStrings, err := redis.Strings(rds.Do("ZPOPMIN", "email"))
+	if err != nil {
+		log.Panic(err)
+	}
+	if len(redisStrings) < 2 {
+		log.Panic("No Entries In Database!")
+	}
+
+	emailAddress := redisStrings[0]
+	alarm, err := strconv.ParseInt(redisStrings[1], 10, 64)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	for _, emailAddress := range emailAddresses {
+	if alarm < now {
 		if emailAddress == "" {
-			continue
+			log.Println("Empty Email Address")
+		} else {
+			util.SendEmail(emailAddress)
 		}
-		util.SendEmail(emailAddress)
-		rows, err := redis.Int(rds.Do("ZREM", "email", emailAddress))
+		return true
+	} else {
+		rows, err := redis.Int(rds.Do("ZADD", "email", "CH", alarm, emailAddress))
 		if err != nil || rows < 1 {
-			log.Printf("Failed to remove email %s: %s\n", emailAddress, err)
+			log.Panicf("Failed to set Watchdog.Email timer for %s: %s", emailAddress, err)
 		}
+		return false
 	}
 }
 
@@ -34,7 +49,8 @@ func main() {
 	log.Println("Watchdog.Email Worker Starting")
 	redisContext := util.NewRedisController()
 	log.Println("Watchdog.Email Worker Running")
-	checkWatchdog(redisContext)
+	for checkWatchdog(redisContext) {
+	}
 	redisContext.CloseRedisController()
 	log.Println("Watchdog.Email Worker Exiting")
 }
