e6942a2126c2e3557637bb50929b47eb689900d9
Jarek Lupinski <jarek@lupin.ski>
2021-01-06 20:03:43 -0500

    use Redis Pool

--- util/redis.go
+++ util/redis.go
@@ -10,6 +10,7 @@ func NewPool(addr string) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
+		Wait:        true,
 		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", addr) },
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			if time.Since(t) < time.Minute {
